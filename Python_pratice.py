88b144bfa813dce54acd2f3809730817f7a3b05d
Dhileepan C R <dhileepan@Dhileepans-MacBook-Pro.local>
2022-01-23 13:02:20 -0600

    first commit

---
+++ Python_pratice.py
@@ -0,0 +1,101 @@
+# print("Hello World!")
+
+# print(type(3))
+
+# ballots = 1,341
+
+# print(type(ballots))
+
+# print(type(''))
+
+# print(help("keywords"))
+
+# # How many votes did you get?
+# my_votes = int(input("How many votes did you get in the election? "))
+# #  Total votes in the election
+# total_votes = int(input("What is the total votes in the election? "))
+# # Calculate the percentage of votes you received.
+# percentage_votes = (my_votes / total_votes) * 100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
+
+# temperature = int(input("What is the temperature outside? "))
+
+# if temperature > 80:
+#     print("Turn on the AC.")
+# else:
+#     print("Open the windows.")
+
+# #What is the score?
+# score = int(input("What is your test score? "))
+
+# # Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+# else:
+#     if score >= 80:
+#         print('Your grade is a B.')
+#     else:
+#         if score >= 70:
+#             print('Your grade is a C.')
+#         else:
+#             if score >= 60:
+#                 print('Your grade is a D.')
+#             else:
+#                 print('Your grade is an F.')
+
+# # What is the score?
+# score = int(input("What is your test score? "))
+
+# # Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+# elif score >= 80:
+#     print('Your grade is a B.')
+# elif score >= 70:
+#     print('Your grade is a C.')
+# elif score >= 60:
+#     print('Your grade is a D.')
+# else:
+#     print('Your grade is an F.')
+
+# counties = ["Arapahoe","Denver","Jefferson"] 
+# if "Arapahoe" in counties: print("True") 
+# else: print("False")
+
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if "El Paso" in counties:
+#     print("El Paso is in the list of counties.")
+# else:
+#     print("El Paso is not the list of counties.")
+
+# for x in counties:
+#     print(x)
+
+# numbers = [0, 1, 2, 3, 4]
+# for num in numbers:
+#     print(num)
+
+# for num in range(5):
+#     print(num)
+
+# for i in range(len(counties)):
+#     print(counties[i])
+
+# counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+# for county in counties_dict:
+#     print(county)
+
+# for county, voters in counties_dict.items():
+#     print(county, voters)
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
